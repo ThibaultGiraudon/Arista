c61e4543126986528fb5c11ebfe3ef577976d06d
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-05-04 13:47:06 +0200

    Add defaultData

---
+++ Arista/Views/IntensityPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  IntensityPickerView.swift
+//  Arista
+//
+//  Created by Thibault Giraudon on 04/05/2025.
+//
+
+import SwiftUI
+
+struct IntensityPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntensityPickerView()
+}
