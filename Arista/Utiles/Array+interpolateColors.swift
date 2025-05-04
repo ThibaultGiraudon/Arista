e1830b3eb8a61072290c65c470b61167c44abb57
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-05-04 14:00:08 +0200

    Move extensions in dedicated files

---
+++ Arista/Utiles/Array+interpolateColors.swift
@@ -0,0 +1,8 @@
+//
+//  Array+interpolateColors.swift
+//  Arista
+//
+//  Created by Thibault Giraudon on 04/05/2025.
+//
+
+import Foundation
---
+++ Arista/Utiles/Color+init.swift
@@ -0,0 +1,8 @@
+//
+//  Color+init.swift
+//  Arista
+//
+//  Created by Thibault Giraudon on 04/05/2025.
+//
+
+import Foundation
