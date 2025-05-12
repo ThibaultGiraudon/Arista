240d1b538def9158d27ab02ec35b83f7b71ddfbd
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-05-12 18:10:38 +0200

    Add new defaultData

--- Arista/Models/DefaultData.swift
+++ Arista/Models/DefaultData.swift
@@ -20,6 +20,11 @@ struct DefaultData {
         
         let sleepRepository = SleepRepository(viewContext: viewContext)
         
+        let exerciseReporsitory = ExerciceRepository(viewContext: viewContext)
+        
+        let calendar = Calendar.current
+        let year = 2025
+        
         deleteAllData(viewContext)
         
         if (try? userRepository.getUser()) == nil {
@@ -27,20 +32,26 @@ struct DefaultData {
             initialUser.name = "Thibault Giraudon"
             initialUser.email = "thibault.giraudon@gmail.com"
             initialUser.password = "test123"
+            initialUser.weight = 76.25
+            initialUser.hoursSleep = 8
+            initialUser.size = 181
             
 
             if try sleepRepository.getSleepSessions().isEmpty {
                 let sleepData: [(day: Int, month: Int, hour: Int, minute: Int, duration: Int)] = [
-                    (5, 05 ,0, 43, 480),   // 8h00
-                    (5, 05, 23, 57, 405),  // 6h45
-                    (7, 05, 0, 30, 510),   // 8h30
-                    (7, 05, 22, 50, 465),  // 7h45
-                    (8, 05, 23, 10, 420),  // 7h00
-                    (10, 05, 0, 5, 450)    // 7h30
+                    (5, 05 ,0, 43, 480),
+                    (5, 05, 23, 57, 405),
+                    (7, 05, 0, 30, 510),
+                    (7, 05, 22, 50, 465),
+                    (8, 05, 23, 10, 420),
+                    (10, 05, 0, 5, 450),
+                    (10, 05, 23, 37, 360),
+                    (11, 05, 22, 00, 305),
+                    (13, 05, 1, 15, 420),
+                    (13, 05, 21, 07, 180),
+                    (14, 05, 23, 57, 450)
+                    
                 ]
-
-                let calendar = Calendar.current
-                let year = 2025
                 
                 for data in sleepData {
                     let sleep = Sleep(context: viewContext)
@@ -62,6 +73,37 @@ struct DefaultData {
                     sleep.duration = Int64(data.duration)
                     sleep.quality = Int16.random(in: 0...10)
                     sleep.user = initialUser
+                }
+            }
+            
+            if try exerciseReporsitory.getExercices().isEmpty {
+                let exerciseData: [(day: Int, month: Int, category: String, intensity: Int16, duration: Int64)] = [
+                    (30, 04, "Boxe", 10, 60),
+                    (10, 05, "Badminton", 3, 63),
+                    (12, 05, "Course à pied", 8, 29),
+                    (12, 05, "Course à pied", 5, 15),
+                ]
+                
+                for data in exerciseData {
+                    let exercise = Exercice(context: viewContext)
+                    
+                    let dateComponents = DateComponents(
+                        calendar: calendar,
+                        timeZone: TimeZone.current,
+                        year: year,
+                        month: data.month,
+                        day: data.day,
+                        hour: 12,
+                        minute: 0,
+                        second: 0
+                    )
+                    
+                    guard let date = calendar.date(from: dateComponents) else { continue }
+                    
+                    exercise.date = date
+                    exercise.category = data.category
+                    exercise.duration = data.duration
+                    exercise.intensity = data.intensity
                 }
             }
              
