ee8a174fcc32bfc3c6528d111825464000abfcba
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-05-12 18:11:15 +0200

    Add categoryItem and calories in Exercise

--- Arista/Models/Exercice.swift
+++ Arista/Models/Exercice.swift
@@ -8,4 +8,18 @@
 import Foundation
 import CoreData
 
-class Exercice: NSManagedObject { }
+class Exercice: NSManagedObject {
+    var categoryItem: Category {
+        for item in Category.allCases {
+            if item.rawValue == self.category {
+                return item
+            }
+        }
+        return .other
+    }
+    
+    var calories: Int {
+        let durationHour: Double = Double(self.duration) / 60.0
+        return Int(self.categoryItem.met * durationHour * (self.user?.weight ?? 75.0))
+    }
+}
