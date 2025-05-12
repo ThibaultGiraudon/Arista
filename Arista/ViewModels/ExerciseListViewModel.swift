9293c94591bcd35e6dd7cd3221792a4d5f0c8b8a
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-05-12 18:12:48 +0200

    Add average duration and calOfDay in ExerciseListViewodel

--- Arista/ViewModels/ExerciseListViewModel.swift
+++ Arista/ViewModels/ExerciseListViewModel.swift
@@ -12,18 +12,33 @@ import CoreData
 class ExerciseListViewModel: ObservableObject {
     @Published var exercises: [Exercice] = []
     @Published var exercisesPerMonth: [Date: [Exercice]] = [:]
+    var averageDuration: Int {
+        var total = 0
+        exercises.forEach({ total += Int($0.duration) })
+        return total / exercises.count
+    }
+    var calOfDay: Int {
+        var total = 0
+        for exercise in exercises {
+            guard let date = exercise.date else { continue }
+            if date.formatted("d MMMM") == Date.now.formatted("d MMMM") {
+                total += exercise.calories
+            }
+        }
+        return total
+    }
 
     var viewContext: NSManagedObjectContext
 
-    init(context: NSManagedObjectContext) {
+    init(context: NSManagedObjectContext = PersistenceController.shared.container.viewContext) {
         self.viewContext = context
         fetchExercises()
     }
 
     func fetchExercises() {
         do {
-            let components = try ExerciceRepository().getExercices()
-            exercisesPerMonth = Dictionary(grouping: components) { exercise -> Date in
+            exercises = try ExerciceRepository().getExercices()
+            exercisesPerMonth = Dictionary(grouping: exercises) { exercise -> Date in
                 let components = Calendar.current.dateComponents([.year, .month], from: exercise.date ?? .now)
                 if let date = Calendar.current.date(from: components) {
                     return date
