de60eb977b78e6b66db4c1c79705dcfd1c1aafd4
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-05-12 18:19:16 +0200

    Add new atributes in User model and UserDataViewModel, Add picker in UserDataView to set new attributes

--- Arista/ViewModels/UserDataViewModel.swift
+++ Arista/ViewModels/UserDataViewModel.swift
@@ -11,10 +11,22 @@ import CoreData
 class UserDataViewModel: ObservableObject {
     @Published var name: String = ""
     @Published var email: String = ""
+    @Published var weight: Double = 75.0
+    @Published var size: Int = 175
+    @Published var hoursSleep: Int = 8
+    
+    var initials: String {
+        let names = self.name.split(separator: " ")
+        var initials = ""
+        for sub in names {
+            initials += sub.capitalized.prefix(1)
+        }
+        return initials
+    }
 
     private var viewContext: NSManagedObjectContext
 
-    init(context: NSManagedObjectContext) {
+    init(context: NSManagedObjectContext = PersistenceController.shared.container.viewContext) {
         self.viewContext = context
         fetchUserData()
     }
@@ -26,6 +38,9 @@ class UserDataViewModel: ObservableObject {
             }
             self.email = user.email ?? ""
             self.name = user.name ?? ""
+            self.weight = user.weight
+            self.size = Int(user.size)
+            self.hoursSleep = Int(user.hoursSleep)
         } catch {
             print(error.localizedDescription)
         }
--- Arista/Views/UserDataView.swift
+++ Arista/Views/UserDataView.swift
@@ -9,25 +9,61 @@ import SwiftUI
 
 struct UserDataView: View {
     @ObservedObject var viewModel: UserDataViewModel
+    let weightValues: [Double] = stride(from: 30.0, through: 150, by: 0.5).map { Double(round(100*$0)/100) }
+    let sizeValues: [Int] = stride(from: 100, to: 230, by: 1).map { $0 }
+    let sleepValue: [Int] = stride(from: 6, to: 14, by: 1).map { $0 }
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Spacer()
-            Text("Hello")
-                .font(.largeTitle)
-            Text("\(viewModel.name)")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(.blue)
-                .padding()
-                .scaleEffect(1.2)
-                .animation(.easeInOut(duration: 1.5).repeatForever(autoreverses: true), value: UUID())
-            Spacer()
+        Form {
+            HStack {
+                Text(viewModel.initials)
+                    .font(.title)
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background {
+                        Circle()
+                            .fill(.gray)
+                    }
+                VStack {
+                    TextField("Nom", text: $viewModel.name)
+                        .font(.title3.bold())
+                    TextField("Email", text: $viewModel.email)
+                }
+            }
+            .listRowBackground(Color("OffWhite"))
+            Section("Renseignements") {
+                Picker("Poid (kg)", selection: $viewModel.weight) {
+                    ForEach(weightValues, id: \.self) { weight in
+                        Text(String(format: "%0.2f kg", weight))
+                    }
+                }
+                .background {
+                    Color("OffWhite")
+                }
+                Picker("Taille (cm)", selection: $viewModel.size) {
+                    ForEach(sizeValues, id: \.self) { size in
+                        Text("\(size) cm")
+                    }
+                }
+                Picker("Heures de repos", selection: $viewModel.hoursSleep) {
+                    ForEach(sleepValue, id: \.self) { sleep in
+                        Text("\(sleep)h")
+                    }
+                }
+            }
+            .listRowBackground(Color("OffWhite"))
         }
-        .edgesIgnoringSafeArea(.all)
+        .scrollContentBackground(.hidden)
+        .background {
+            Color("DimGray")
+                .ignoresSafeArea()
+        }
+        .foregroundStyle(Color("TextColor"))
     }
 }
 
 #Preview {
-    UserDataView(viewModel: UserDataViewModel(context: PersistenceController.preview.container.viewContext))
+    NavigationStack {
+        UserDataView(viewModel: UserDataViewModel())
+    }
 }
